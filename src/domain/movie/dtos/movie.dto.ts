28da23ee47918be5f362373a1abb4f8416a8e9c9
reisdiego42 <reisdiego144@gmail.com>
2023-11-25 16:09:04 -0300

    adicionando dtos basicos

---
+++ src/domain/movie/dtos/movie.dto.ts
@@ -0,0 +1,52 @@
+import { Type } from 'class-transformer';
+import {
+  ArrayNotEmpty,
+  IsArray,
+  IsNotEmpty,
+  IsString,
+  IsUUID,
+  Max,
+  MaxLength,
+  Min,
+} from 'class-validator';
+import { MovieEnum } from 'src/domain/movie/enums';
+
+export class MovieDto {
+  @IsUUID()
+  @IsNotEmpty()
+  id: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(MovieEnum.MAX_NAME_LENGTH)
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(MovieEnum.MAX_DIRECTOR_NAME_LENGTH)
+  director: string;
+
+  @IsArray()
+  @ArrayNotEmpty()
+  @IsString({ each: true })
+  @MaxLength(MovieEnum.MAX_CATEGORY_NAME_LENGTH, { each: true })
+  category: string[];
+
+  @IsArray()
+  @ArrayNotEmpty()
+  @IsString({ each: true })
+  @MaxLength(MovieEnum.MAX_ACTORS_NAME_LENGTH, { each: true })
+  actors: string[];
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(MovieEnum.MAX_DESCRIPTION_LENGTH)
+  description: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Type(() => Number)
+  @Max(MovieEnum.MAX_YEAR)
+  @Min(MovieEnum.MIN_YEAR)
+  year: number;
+}
