f4fd8e0ac415878d4e0f692914ff425d8708a3ef
reisdiego42 <reisdiego144@gmail.com>
2023-11-25 15:16:44 -0300

    adicionando app.module

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DomainModule } from 'src/domain';
+import { User } from 'src/domain/user/entities';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'pass123',
+      database: 'testdb',
+      entities: [User],
+      synchronize: true,
+    }),
+    DomainModule,
+    ConfigModule.forRoot(),
+  ],
+})
+export class AppModule {}
